9940f1d039433ce28cda6528f6173ef11b8fb4fb
Ong Sze Swee <szeswee165@gmail.com>
2017-07-09 19:44:41 +0800

    Add new imaging library for compressing photos; please run `pip2 install Pillow` immediately after pulling this commit

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from flaskext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 from datetime import datetime
 from forms import LoginForm, RetrievalForm, AddUserForm, CreateNewItem,AddNewLocation,ExistingItemsLocation
-import os, copy, re, csv, json_decode
+import os, copy, re, csv, json_decode, imaging
 # from flask.ext.cache import Cache
 
 
@@ -21,7 +21,7 @@ import os, copy, re, csv, json_decode
 # pip2 install scipy
 # pip2 install statsmodels
 # pip2 install pandas
-# pip2 install PIL
+# pip2 install Pillow
 # eb init -p python2.7 aim
 # eb init
 # eb create flask-env
@@ -668,9 +668,9 @@ def admin():
 
 
                 if 'photo' in request.files:
-                    filename =photos.save(request.files['photo'])
+                    filename = photos.save(request.files['photo'])
 
-                item = [itemname, category,filename, price, reorderpt, out_by,in_by,in_out_ratio]
+                item = [itemname, category, filename, price, reorderpt, out_by, in_by, in_out_ratio]
                 print(item)
                 try:
                     # TODO: string parameterisation
--- imaging.py
+++ imaging.py
@@ -1,30 +1,16 @@
 from PIL import Image
+import os, sys
 
-def cropImg(file):
-    
-    img = Image.open(file)
-
-    # if width > height
-    if img.size[0] > img.size[1]:
-        b = ((img.size[0] - img.size[1])/2,
-             img.size[0] - ((img.size[0] - img.size[1])/2))
-        img2 = img.crop((b[0], 0, b[1], img.size[1]))
-        img2.save(file)
-    
-    # if width < height
-    elif img.size[0] < img.size[1]:
-        b = ((img.size[1] - img.size[0])/2,
-             img.size[1] - ((img.size[1] - img.size[0])/2))
-        img2 = img.crop((0, b[0], img.size[0], b[1]))
-        img2.save(file)
-
-def resizeImg(file, height=200):
-    img = Image.open(file)
-    if img.size[0] > height:
-        hpercent = (height / float(img.size[1]))
-        wsize = int((float(img.size[0]) * float(hpercent)))
-        img = img.resize((wsize, height), PIL.Image.ANTIALIAS)
-        img.save(file)
+def thumb(size=285, *args):
+    for infile in args:
+        outfile = os.path.splitext(infile)[0] + ".thumbnail"
+        if infile != outfile:
+            try:
+                im = Image.open(infile)
+                im.thumbnail((size, size), Image.ANTIALIAS)
+                im.save(outfile, "JPEG")
+            except IOError:
+                print "PYTHON: Cannot create thumbnail for '%s'" % infile
 
 if __name__ == '__main__':
-    print(">>> This is an imaging library and is not meant to be executed directly")
+    print("PYTHON: `Imaging.py` is not meant to be executed directly")
