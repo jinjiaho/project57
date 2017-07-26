785786a5c93d32a41946a3a620630f71d342bc2a
Ong Sze Swee <szeswee165@gmail.com>
2017-07-26 12:33:57 +0800

    Change server configs

--- wsgi_dogfood/wsgi_dogfood.py
+++ wsgi_dogfood/wsgi_dogfood.py
@@ -2,5 +2,5 @@ from application import application
 
 if __name__ == '__main__':
 	application.config.from_object('config.Config')
-	application.config.from_pyfile('amazonRDS.py')
+	application.config.from_pyfile('dogfood.py')
 	application.run()
