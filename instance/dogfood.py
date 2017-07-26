440cbf511921f2cf730d923812d65160d2348bcd
Ong Sze Swee <szeswee165@gmail.com>
2017-07-26 11:25:43 +0800

    Add private test configuration

---
+++ instance/dogfood.py
@@ -0,0 +1,20 @@
+SECRET_KEY='development-key'
+MYSQL_DATABASE_USER='root'
+MYSQL_DATABASE_PASSWORD='classroom'
+MYSQL_DATABASE_DB='Ascott_InvMgmt'
+MYSQL_DATABASE_HOST='dogfood.coxb3venarbl.ap-southeast-1.rds.amazonaws.com:'
+
+
+UPLOADS_DEFAULT_DEST = 'static/img/items/'
+UPLOADS_DEFAULT_URL = 'http://localhost:5000/static/img/items/'
+
+UPLOADED_IMAGES_DEST = 'static/img/items/'
+UPLOADED_IMAGES_URL = 'http://localhost:5000/static/img/items/'
+
+BABEL_LOCALES = ('en', 'zh', 'ms', 'ta')
+BABEL_DEFAULT_LOCALE = "en"
+
+# Property-specific info
+# Timezone string follows TZ format (see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
+TIMEZONE = "Asia/Singapore"
+PROP_NAME = "Capstone Room"
